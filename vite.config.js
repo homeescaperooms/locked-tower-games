7b100ee91dc52368365a5b192f020d17eed46157
Leon C. <leonch@live.de>
2024-06-28 14:54:00 +0200

    Add tower subdomain

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
     base: "/tower/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                solved: resolve(__dirname, 'solved.html'),
+                gameHateSpeech: resolve(__dirname, 'game/hate_speech.html')
+            },
+        },
+    },
 });
