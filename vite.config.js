ef7386bd7431a8d394115e2247c7d8c222cc093e
Leon C. <leonch@live.de>
2024-07-03 11:24:37 +0200

    Add entrypoint for game 3

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 solved: resolve(__dirname, "solved.html"),
                 gameHateSpeech: resolve(__dirname, "game/hate_speech.html"),
                 gameUnknownLanguage: resolve(__dirname, "game/unknown_language.html"),
+                gameEmotions: resolve(__dirname, "game/emotions.html"),
             },
         },
     },
